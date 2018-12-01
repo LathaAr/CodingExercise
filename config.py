547c03463c0c90b025c80930895b26e9362436f6
LathaAr <45244675+LathaAr@users.noreply.github.com>
2018-12-01 20:19:00 +0530

    Monitor and Display Wireless APs modification

---
+++ config.py
@@ -0,0 +1,21 @@
+# This is config file which contains surrounding Wireless APs in JSON format
+config_parser = {
+  "access_points": [
+    {
+      "ssid": "MyAP",
+      "snr": 82,
+      "channel": 11
+    },
+    {
+      "ssid": "YourAP",
+      "snr": 42,
+      "channel": 1
+    },
+    {
+      "ssid": "HerAP",
+      "snr": 71,
+      "channel": 1
+    }
+  ]
+}
+
